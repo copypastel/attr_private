aeb61ef15ce4522ece19a76b9efd12030e0606f7
daicoden <daicoden@copypastel.com>
2009-11-29 14:50:17 -0700

    Removed the method definitions.  Originally tried to define these private methods where all the other setters and getters are defined.  For some reason the private statement was always ignored.

--- lib/attr_private.rb
+++ lib/attr_private.rb
@@ -3,7 +3,4 @@
 lib = File.expand_path(File.dirname(__FILE__) + '/attr_private')
 $:.unshift(lib) unless $:.include? lib
 
-
-
 require 'active_record_base'
-#require 'active_record_attribute_methods'
