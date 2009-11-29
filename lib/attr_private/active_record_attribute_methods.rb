974a21c4c2c9087af1953172c4f836fbb79181a9
daicoden <daicoden@copypastel.com>
2009-11-29 09:33:46 -0700

    Got the Read to be overwritten finaly.

--- lib/attr_private/active_record_attribute_methods.rb
+++ lib/attr_private/active_record_attribute_methods.rb
@@ -1,6 +1,23 @@
 module ActiveRecord::AttributeMethods::ClassMethods
   private
   def define_read_method(symbol,attr_name,column)
-    raise "FUCK ME SON OF A BITCH"
+    cast_code = column.type_cast_code('v') if column
+    access_code = cast_code ? "(v=@attributes['#{attr_name}']) && #{cast_code}" : "@attributes['#{attr_name}']"
+
+    unless attr_name.to_s == self.primary_key.to_s
+      access_code = access_code.insert(0, "missing_attribute('#{attr_name}', caller) unless @attributes.has_key?('#{attr_name}'); ")
+    end
+    
+    if cache_attribute?(attr_name)
+      access_code = "@attributes_cache['#{attr_name}'] ||= (#{access_code})"
+    end
+    unless private_attributes.include?(attr_name)
+      evaluate_attribute_method attr_name, "def #{symbol}; #{access_code}; end"
+    else
+      method = "def #{symbol} 
+        raise NoMethodError
+      end"
+      evaluate_attribute_method attr_name, method 
+    end
   end
 end
